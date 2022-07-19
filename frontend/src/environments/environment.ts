f85338b82c49ffe19e2c8f184c89f3b9a5e59de2
Samuel Nzubechi Chukwuma <98186867+denzubechi@users.noreply.github.com>
2022-07-18 19:09:18 -0700

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -7,8 +7,8 @@ export const environment = {
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
     url: 'coffeeshopf', // the auth0 domain prefix
-    audience: 'coffee', // the audience set for the auth0 app
-    clientId: 'zzUoxZT1mmHkLHC2eQ15Qa5wkWXkQwRD', // the client id generated for the auth0 app
+    audience: 'coffee', // the audience set for the/your auth0 app
+    clientId: 'zzUoxZT1mmHkLHC2eQ15Qa5wkWXkQwRD', // the client id generated for the/your auth0 app
     callbackURL: 'https://127.0.0.1:8100', // the base url of the running ionic application. 
   }
 };
